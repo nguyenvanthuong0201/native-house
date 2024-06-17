15ce32788b41053ef37ef40acc36f699a4015ecb
ThuongNguyen <thuongnv@starboard-asia.com>
2024-06-17 20:44:30 +0700

    first commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,70 +1,67 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import React, { useRef, Suspense, useState, useEffect } from 'react';
+import { Canvas, useFrame, useLoader, useThree } from '@react-three/fiber/native';
+import { Environment } from '@react-three/drei/native';
+import { GLTFLoader } from 'three-stdlib';
+import { Asset } from 'expo-asset';
+import useControls from "r3f-native-orbitcontrols";
+import { View } from "react-native";
+import { PerspectiveCamera } from 'three';
+import { useDrag } from '@use-gesture/react';
+import { useSpring, animated } from "@react-spring/three";
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+function Model(props: any) {
+  const asset = Asset.fromModule(require('../../assets/models/house_building_home.glb')).uri;
+  const house = useLoader(GLTFLoader, asset);
+  return <primitive {...props} object={house.scene} position={[-35, -10, 0]} />;
+}
 
-export default function HomeScreen() {
+function DraggableModel(props: any) {
+  const ref = useRef();
+  const { viewport } = useThree();
+  const [active, setActive] = useState(false);
+  const [  , api] = useSpring(() => ({
+    position: [0, 0, 0]
+  }));
+
+  const bind = useDrag(({ offset: [offsetX,offsetY] }) => {
+    const translateX = offsetX / viewport.factor;
+    const translateY = -offsetY / viewport.factor;
+    api.start({ position: [translateX, translateY, 0] });
+  });
+
+  const asset = Asset.fromModule(require('../../assets/models/tree.glb')).uri;
+  const model = useLoader(GLTFLoader, asset);
+
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <primitive
+      {...props}
+      object={model.scene}
+      ref={ref}
+      {...bind()}
+      scale={active ? 0.1 : 0.2}
+      onClick={() => setActive(!active)}
+    />
   );
 }
 
-const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
-});
+export default function App() {
+  const [OrbitControls, events] = useControls();
+  const [isDragging, setIsDragging] = useState(false);
+  const camera = new PerspectiveCamera();
+  camera.position.set(0, 2, 5);
+  camera.lookAt(0, 0, 0);
+
+  return (
+    <View style={{ flex: 1 }} {...events}>
+      <Canvas camera={camera}>
+        <ambientLight />
+        <Suspense fallback={null}>
+          <Model />
+          <DraggableModel setIsDragging={setIsDragging} />
+          <Environment preset="dawn" background />
+        </Suspense>
+        <OrbitControls enableRotate={!isDragging} />
+      </Canvas>
+    </View>
+  );
+}
